daad62ddb467d1d2e1f0684599f6336acde49094
Jbrod5 <jorgebravo202131782@cunoc.edu.gt>
2024-02-28 01:00:47 -0600

    Proyecto NB

---
+++ IDE_SQL/src/main/java/com/mycompany/ide_sql/IDE_SQL.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ide_sql;
+
+/**
+ *
+ * @author jbravo
+ */
+public class IDE_SQL {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
